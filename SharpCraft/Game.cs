ebe1a13715309c501600b94c94e0b63a57edfa1e
Unknown <tom@dlnet.us>
2018-03-22 16:58:20 +0100

    commit 1

---
+++ SharpCraft/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using OpenTK;
+
+namespace SharpCraft
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool FreeConsole();
+
+        static void Main(string[] args)
+        {
+            FreeConsole();
+
+            var wnd = new Window();
+
+            wnd.Run(60);
+        }
+
+        class Window : GameWindow
+        {
+            public Window()
+            {
+                Title = "Kokot na kaši";
+            }
+
+            protected override void OnRenderFrame(FrameEventArgs e)
+            {
+
+            }
+        }
+    }
+}
---
+++ SharpCraft/shader/ShaderProgram.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenG_Basics
+{
+    class ShaderProgram
+    {
+        private int programID;
+
+        private int vshID;
+        private int fshID;
+
+        public ShaderProgram(string shaderName)
+        {
+            loadShader(shaderName);
+
+            programID = GL.CreateProgram();
+
+            GL.AttachShader(programID, vshID);
+            GL.AttachShader(programID, fshID);
+
+            //bindAttributes();
+
+            GL.LinkProgram(programID);
+            GL.ValidateProgram(programID);
+
+            //getAllUniformLocations();
+
+            //ShaderManager.registerShader(this);
+        }
+
+        private void loadShader(string shaderName)
+        {
+            string vshCode = File.ReadAllText($"assets/shader/{shaderName}.vsh");
+            string fshCode = File.ReadAllText($"assets/shader/{shaderName}.fsh");
+
+            vshID = GL.CreateShader(ShaderType.VertexShader);
+            fshID = GL.CreateShader(ShaderType.FragmentShader);
+
+            GL.ShaderSource(vshID, vshCode);
+            GL.ShaderSource(fshID, fshCode);
+
+            GL.CompileShader(vshID);
+            GL.CompileShader(fshID);
+        }
+
+        public void bind()
+        {
+            GL.UseProgram(programID);
+        }
+
+        public void unbind()
+        {
+            GL.UseProgram(0);
+        }
+        
+        public void destroy()
+        {
+            unbind();
+
+            GL.DetachShader(programID, vshID);
+            GL.DetachShader(programID, fshID);
+
+            GL.DeleteShader(vshID);
+            GL.DeleteShader(fshID);
+
+            GL.DeleteProgram(programID);
+        }
+    }
+}
