b052bd56d3b61d31be6d76159e928a5c4071395e
LapisSea <toni.tube.you@gmail.com>
2018-04-01 22:20:12 +0200

    derpy fix fixy fux

--- SharpCraft/SharpCraft_Data/assets/shaders/block.fsh
+++ SharpCraft/SharpCraft_Data/assets/shaders/block.fsh
@@ -6,14 +6,13 @@ in float brightness;
 out vec4 out_Color;
 
 uniform sampler2D textureSampler;
-uniform vec3 lightColor;
 
 void main(void){
 	vec4 pixelColor = texture(textureSampler, pass_textureCoords);
 	
 	if(pixelColor.a == 0)discard;
 	
-	vec3 diffuse = brightness * lightColor;
+	vec3 diffuse = brightness;
 	
 	out_Color = vec4(diffuse, 1.0) * pixelColor;
 }
--- SharpCraft/render/shader/module/ShaderModule3D.cs
+++ SharpCraft/render/shader/module/ShaderModule3D.cs
@@ -7,8 +7,10 @@ namespace SharpCraft.render.shader.module
 {
 	public class ShaderModule3D<T> : ShaderModule<T>
 	{
-	    private UniformMat4 Projection;//TODO add the View matrtix 
+		private UniformMat4 Projection;
 		private UniformMat4 Transform;
+		private UniformMat4 View;
+
 
 		public ShaderModule3D(Shader<T> parent) : base(parent)
 		{
@@ -20,20 +22,27 @@ namespace SharpCraft.render.shader.module
 			{
 				Projection = new UniformMat4(Parent.GetUniformId("projectionMatrix"));
 			}
-			catch
+			catch (Exception e)
 			{}
+			try
+			{
+				View = new UniformMat4(Parent.GetUniformId("viewMatrix"));
+			}
+			catch (Exception e)
 			{}
 			try
 			{
 				Transform = new UniformMat4(Parent.GetUniformId("transformationMatrix"));
 			}
-			catch
+			catch (Exception e)
 			{}
 		}
 
 		public override void UpdateGlobalUniforms()
 		{
 			Projection?.Update(SharpCraft.Instance.CreateProjectionMatrix());
+			View?.Update(SharpCraft.Instance.Camera.View);
+
 		}
 
 		public override void UpdateInstanceUniforms(Matrix4 transform, T renderable)
