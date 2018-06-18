8296ebf01da209e097aeb521ff2bec2bd245ab66
Unknown <tom@dlnet.us>
2018-06-19 01:58:14 +0200

    BlockRegistry optimization

--- SharpCraft/block/BlockRegistry.cs
+++ SharpCraft/block/BlockRegistry.cs
@@ -6,12 +6,12 @@ namespace SharpCraft.block
     internal class BlockRegistry
     {
         private static Dictionary<string, Block> _registry = new Dictionary<string, Block>();
-        private static Dictionary<Type, Block> _typeRegistry = new Dictionary<Type, Block>();
+        private static Dictionary<Type, string> _typeRegistry = new Dictionary<Type, string>();
 
         public void Put(Block b)
         {
             _registry.Add(b.UnlocalizedName, b);
-            _typeRegistry.Add(b.GetType(), b);
+            _typeRegistry.Add(b.GetType(), b.UnlocalizedName);
         }
 
         public void RegisterBlocksPost()
@@ -24,7 +24,7 @@ namespace SharpCraft.block
 
         public static Block GetBlock<TBlock>() where TBlock : Block
         {
-            return _typeRegistry[typeof(TBlock)];
+            return _registry[_typeRegistry[typeof(TBlock)]];
         }
 
         public static Block GetBlock(string unlocalizedName)
